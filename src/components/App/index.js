3117ed6313805331127adf53b065c68262e18f1b
AliceCkn <choukroun.alice@gmail.com>
2022-05-03 13:48:18 +0200

    configuration of craco

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,11 @@
 import './app.scss';
+import logo from '@/assets/images/logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='{}' className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
