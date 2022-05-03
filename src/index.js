81d3dbe7d332a252bf07e72c7240247737002834
AliceCkn <choukroun.alice@gmail.com>
2022-05-03 14:31:57 +0200

    Configuration of redux:done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
 import store from '@/store';
 import App from '@/components/App';
-
 import '@/styles/index.scss';
 import reportWebVitals from './reportWebVitals';
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
 
-import reducer from 'src/reducers';
-// on importe notre middleware
+import reducer from '@/reducers';
 import boardgameatlasApiMiddleWare from '../middlewares/boardgameatlasApi';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancers = composeEnhancers(
-  // on le branche sur le store
   applyMiddleware(boardgameatlasApiMiddleWare),
 );
 
