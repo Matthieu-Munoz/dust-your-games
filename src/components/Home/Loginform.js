9de25a7218b1f963b478aa9fb9a33207a45c13c8
Matthieu-Munoz <matthieu.munoz.perso@gmail.com>
2022-05-09 12:01:02 +0200

    Setting up loggin api

--- src/components/Home/Loginform.js
+++ src/components/Home/Loginform.js
@@ -1,25 +1,36 @@
 // Dependencies
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { BsPerson } from "react-icons/bs";
 import { AiOutlineLock } from "react-icons/ai";
 // React-Redux
-import Field from "./Field";
-import { toggleLoginForm } from '../../actions/home';
+import Field from "@/components/Field";
+import { toggleLoginForm } from '@/actions/home';
+import { changeUserField, login } from '@/actions/user';
+
 // Styles
 
 function Loginform() {
     const dispatch = useDispatch();
 
+    const email = useSelector((state) => state.user.email);
+    const password = useSelector((state) => state.user.password);
+
     const handleChangeField = (value, name) => {
-        console.log(value, name);
+        dispatch(changeUserField(value, name));
     };
+
+    const handleLogin = (evt) => {
+        evt.preventDefault();
+        dispatch(login())
+    }
+
     return (
         <form className="userform__ctn userform__ctn--login">
             <Field
-                name="pseudo"
+                name="email"
                 placeholder="Identifiant"
                 onChange={handleChangeField}
-                value={''}
+                value={email}
                 Icon={BsPerson}
             />
             <Field
@@ -27,11 +38,11 @@ function Loginform() {
                 type="password"
                 placeholder="Mot de passe"
                 onChange={handleChangeField}
-                value={''}
+                value={password}
                 Icon={AiOutlineLock}
             />
             <a className="userform__ctn__link" href="lien">Mot de passe oublié ?</a>
-            <button className="userform__btn userform__btn--primary" type="submit">Se connecter</button>
+            <button onClick={handleLogin} className="userform__btn userform__btn--primary" type="submit">Se connecter</button>
             <div className="userform__seperator" />
             <button onClick={() => dispatch(toggleLoginForm(false))} className="userform__btn userform__btn--secondary" type="button">S'inscrire</button>
         </form>
