9f7f9317811c4a6e14937b572b544b99a3ed7bd2
Matthieu-Munoz <matthieu.munoz.perso@gmail.com>
2022-05-17 15:57:05 +0200

    fix mobile first, strating layout shift for desktop

--- src/actions/games.js
+++ src/actions/games.js
@@ -2,3 +2,10 @@ export const TOGGLE_FILTER_MENU = 'TOGGLE_FILTER_MENU';
 export const toggleFilterMenu = () => ({
     type: TOGGLE_FILTER_MENU,
 });
+
+export const TOGGLE_FILTER = 'TOGGLE_FILTER';
+export const toggleFilter = (selector, value) => ({
+    type: TOGGLE_FILTER,
+    selector,
+    value,
+});
--- src/reducers/games.js
+++ src/reducers/games.js
@@ -1,10 +1,21 @@
 import {
-    TOGGLE_FILTER_MENU,
+    TOGGLE_FILTER_MENU, TOGGLE_FILTER
 } from '@/actions/games';
 
 export const initialState = {
     games: [],
     menuToggled: false,
+    toggles: {
+        check: false,
+        sortAlpha: false,
+        sortNum: false,
+        sortDir: false,
+        categories: false,
+        times: false,
+        players: false,
+        age: false,
+
+    }
 };
 
 const reducer = (state = initialState, action = {}) => {
@@ -13,6 +24,14 @@ const reducer = (state = initialState, action = {}) => {
             return {
                 ...state,
                 menuToggled: !state.menuToggled,
+            }
+        case TOGGLE_FILTER:
+            return {
+                ...state,
+                toggles: {
+                    ...state.toggles,
+                    [action.selector]: action.value,
+                }
             }
         default:
             return state;
