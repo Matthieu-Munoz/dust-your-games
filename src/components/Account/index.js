d32e90326bf4b7271dd952cc0107442b5dce11c1
Matthieu-Munoz <matthieu.munoz.perso@gmail.com>
2022-05-18 11:24:42 +0200

    starting login check

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,29 +1,32 @@
 // Dependencies
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// Local | React-Redux
+// React-Redux
 import UserAccount from "./UserAccount";
 import EditAccount from "./EditAccount"
-import { useNavigate } from 'react-router-dom';
-import { fetchUser } from '@/actions/user';
+import { Navigate, useLocation } from 'react-router-dom';
 // Styles
 import "./account.scss"
+import { useEffect } from 'react';
+import { loginCheck } from '@/actions/user';
 
 function Account() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   // Control if the edit mode of the account is toggled
   const isEdit = useSelector((state) => state.account.isEdit);
-  const loginChecked = useSelector((state) => state.user.loginChecked);
-
+  // const logged = useSelector((state) => state.user.logged);
+  const dispatch = useDispatch();
+  // if (!logged) {
+  //   return <Navigate to="/" replace />;
+  // }
+  // On location change, scroll the windows to the top and close the menu
+  const location = useLocation();
   useEffect(
     () => {
-      dispatch(fetchUser())
-      if (!loginChecked) {
-        navigate('../', { replace: true });
-      }
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      dispatch(loginCheck())
     },
-    [],
+    [location, dispatch],
   );
   return (
     <div className="account">
