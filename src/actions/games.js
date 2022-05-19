15597724827f92253d29b9c4c3bba11fff720f3c
Matthieu-Munoz <matthieu.munoz.perso@gmail.com>
2022-05-19 13:11:51 +0200

    cleanup function for modals

--- src/actions/games.js
+++ src/actions/games.js
@@ -10,35 +10,12 @@ export const toggleFilter = (selector, value) => ({
     selector,
     value,
 });
-// Handle a change inside an input
-export const CHANGE_FIELD = 'CHANGE_FIELD';
-export const changeField = (value, field) => ({
-    type: CHANGE_FIELD,
-    value,
-    field,
-});
+
 export const FETCH_GAMES = 'FETCH_GAMES';
 export const fetchGames = () => ({
     type: FETCH_GAMES,
 });
 
-
-// Search a game un BGA API
-export const SEARCH_GAME = 'SEARCH_GAME';
-export const searchGame = (value) => ({
-    type: SEARCH_GAME,
-    value,
-});
-export const SAVE_SEARCH_GAMES = 'SAVE_SEARCH_GAMES';
-export const saveSearchGames = (games) => ({
-    type: SAVE_SEARCH_GAMES,
-    games,
-});
-export const SELECT_GAME = 'SELECT_GAME';
-export const selectGame = (value) => ({
-    type: SELECT_GAME,
-    value,
-});
 export const SAVE_GAME = 'SAVE_GAME';
 export const saveGame = (game) => ({
     type: SAVE_GAME,
