f5025b2c91709c345c62a6db44bc116e5e460fd9
AliceCkn <choukroun.alice@gmail.com>
2022-05-20 14:49:13 +0200

    with DOMpurify and some fix

--- src/components/Modal/GameDesc/index.js
+++ src/components/Modal/GameDesc/index.js
@@ -1,4 +1,5 @@
 // Dependencies
+import DOMPurify from "dompurify";
 import { useDispatch, useSelector } from "react-redux";
 import { BsPeopleFill } from "react-icons/bs";
 import { CgSandClock } from "react-icons/cg";
@@ -17,16 +18,13 @@ function GameDesc() {
     const gameFiltered = games.filter(currentGame => {
         return currentGame.game.id === selectedGame;
     });
-    // Dans la variable game, il y a le jeu cliqué.
     const game = gameFiltered[0].game
-
 
     return (
         <div className="gamedesc">
             {modalLoading && <ModalLoader />}
             {!modalLoading &&
                 <>
-                    {/* // votre code ici */}
                     <div className="gamedesc__game">
                         <img
                             className="gamedesc__game__img"
@@ -34,15 +32,7 @@ function GameDesc() {
                             alt={game.name}
                         />
                         <div className='gamedesc--scroll'>
-                            <div className="gamedesc__game__desc">
-                                Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise
-                                en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis
-                                les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour
-                                réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles,
-                                mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié.
-                                Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des
-                                passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise
-                                en page de texte, comme Aldus PageMaker.
+                            <div className="gamedesc__game__desc" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(game.description)}}>
                             </div>
                         </div>
                         <div className="gamedesc__game__information">
