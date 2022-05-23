f9fc0100f7c3ea4f30717f34666e912fa627daa7
AliceCkn <choukroun.alice@gmail.com>
2022-05-23 15:28:33 +0200

    JSX integration: some fix and scss integration + responsive design: done

--- src/components/Modal/FriendsList/index.js
+++ src/components/Modal/FriendsList/index.js
@@ -1,24 +1,22 @@
 // Styles
 import "./friendslist.scss"
-import friendslist from "@/assets/images/friendslist.png";
-
 
 function FriendsList() {
 
     return (
         <div className="friendslist">
                     <div className="friendslist__ctn">
-                        <h2 className="friendslist_ctn_title"> Votre liste d'amis </h2>
+                        <h2 className="friendslist__ctn__title"> Votre liste d'amis </h2>
                         <img
                             className="friendslist__ctn__img"  
                             src={`https://res.cloudinary.com/dyg/image/upload/c_scale,h_300,w_250,q_80/friendslist_nygicd.png`}
                             alt={'friendslist'}
                         />
                     </div>
-                    <div className='friendslist__text'>
-                        C’est ici que vous pourrez accéder
-                        à votre liste d’amis.
-                        Connectez-vous à votre entourage
+                    <div className='friendslist__ctn__text'>
+                        C’est ici que vous pourrez accéder <br />
+                        à votre liste d’amis. <br />
+                        Connectez-vous à votre entourage <br />
                         et accédez à leurs jeux à tout moment !
                     </div>
         </div>
